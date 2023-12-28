dbfdd9644133febea5ad4bf940b6bb17b5197dab
Rean Makara <makara.rean@ubill24.com>
2023-12-28 15:47:38 +0700

    Add project files.

---
+++ MainForm.Designer.cs
@@ -0,0 +1,377 @@
+﻿namespace ExchangeRate
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Title = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtUsd = new System.Windows.Forms.TextBox();
+            this.txtAud = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtBah = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtEur = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtKhr = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnApplyChanges = new System.Windows.Forms.Button();
+            this.txtKhrRate = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtEurRate = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtBahRate = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtAudRate = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Title
+            // 
+            this.Title.AutoSize = true;
+            this.Title.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Title.Location = new System.Drawing.Point(257, 23);
+            this.Title.Name = "Title";
+            this.Title.Size = new System.Drawing.Size(252, 29);
+            this.Title.TabIndex = 0;
+            this.Title.Text = "Exchange Calculator";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(271, 82);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "USD";
+            // 
+            // txtUsd
+            // 
+            this.txtUsd.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUsd.Location = new System.Drawing.Point(345, 79);
+            this.txtUsd.Name = "txtUsd";
+            this.txtUsd.Size = new System.Drawing.Size(123, 26);
+            this.txtUsd.TabIndex = 2;
+            this.txtUsd.KeyUp += new System.Windows.Forms.KeyEventHandler(this.txtUsd_KeyUp);
+            // 
+            // txtAud
+            // 
+            this.txtAud.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAud.Location = new System.Drawing.Point(345, 263);
+            this.txtAud.Name = "txtAud";
+            this.txtAud.Size = new System.Drawing.Size(123, 26);
+            this.txtAud.TabIndex = 4;
+            this.txtAud.KeyUp += new System.Windows.Forms.KeyEventHandler(this.txtAud_KeyUp);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(271, 266);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "AUD";
+            // 
+            // txtBah
+            // 
+            this.txtBah.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBah.Location = new System.Drawing.Point(345, 217);
+            this.txtBah.Name = "txtBah";
+            this.txtBah.Size = new System.Drawing.Size(123, 26);
+            this.txtBah.TabIndex = 6;
+            this.txtBah.KeyUp += new System.Windows.Forms.KeyEventHandler(this.txtBah_KeyUp);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(271, 220);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "BAH";
+            // 
+            // txtEur
+            // 
+            this.txtEur.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEur.Location = new System.Drawing.Point(345, 171);
+            this.txtEur.Name = "txtEur";
+            this.txtEur.Size = new System.Drawing.Size(123, 26);
+            this.txtEur.TabIndex = 8;
+            this.txtEur.KeyUp += new System.Windows.Forms.KeyEventHandler(this.txtEur_KeyUp);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(271, 174);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 20);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "EUR";
+            // 
+            // txtKhr
+            // 
+            this.txtKhr.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtKhr.Location = new System.Drawing.Point(345, 125);
+            this.txtKhr.Name = "txtKhr";
+            this.txtKhr.Size = new System.Drawing.Size(123, 26);
+            this.txtKhr.TabIndex = 10;
+            this.txtKhr.KeyUp += new System.Windows.Forms.KeyEventHandler(this.txtKhr_KeyUp);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(271, 128);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(43, 20);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "KHR";
+            // 
+            // btnReset
+            // 
+            this.btnReset.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReset.Location = new System.Drawing.Point(275, 309);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(193, 30);
+            this.btnReset.TabIndex = 11;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // btnApplyChanges
+            // 
+            this.btnApplyChanges.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnApplyChanges.Location = new System.Drawing.Point(624, 154);
+            this.btnApplyChanges.Name = "btnApplyChanges";
+            this.btnApplyChanges.Size = new System.Drawing.Size(123, 27);
+            this.btnApplyChanges.TabIndex = 23;
+            this.btnApplyChanges.Text = "Apply changes";
+            this.btnApplyChanges.UseVisualStyleBackColor = true;
+            this.btnApplyChanges.Click += new System.EventHandler(this.btnApplyChanges_Click);
+            // 
+            // txtKhrRate
+            // 
+            this.txtKhrRate.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtKhrRate.Location = new System.Drawing.Point(624, 46);
+            this.txtKhrRate.Name = "txtKhrRate";
+            this.txtKhrRate.Size = new System.Drawing.Size(123, 20);
+            this.txtKhrRate.TabIndex = 22;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(588, 49);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(30, 13);
+            this.label6.TabIndex = 21;
+            this.label6.Text = "KHR";
+            // 
+            // txtEurRate
+            // 
+            this.txtEurRate.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEurRate.Location = new System.Drawing.Point(624, 72);
+            this.txtEurRate.Name = "txtEurRate";
+            this.txtEurRate.Size = new System.Drawing.Size(123, 20);
+            this.txtEurRate.TabIndex = 20;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(588, 76);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(30, 13);
+            this.label7.TabIndex = 19;
+            this.label7.Text = "EUR";
+            // 
+            // txtBahRate
+            // 
+            this.txtBahRate.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBahRate.Location = new System.Drawing.Point(624, 98);
+            this.txtBahRate.Name = "txtBahRate";
+            this.txtBahRate.Size = new System.Drawing.Size(123, 20);
+            this.txtBahRate.TabIndex = 18;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(590, 101);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(29, 13);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "BAH";
+            // 
+            // txtAudRate
+            // 
+            this.txtAudRate.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAudRate.Location = new System.Drawing.Point(624, 124);
+            this.txtAudRate.Name = "txtAudRate";
+            this.txtAudRate.Size = new System.Drawing.Size(123, 20);
+            this.txtAudRate.TabIndex = 16;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(590, 127);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(30, 13);
+            this.label9.TabIndex = 15;
+            this.label9.Text = "AUD";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(620, 23);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(132, 20);
+            this.label11.TabIndex = 12;
+            this.label11.Text = "Exchange Rate";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(753, 49);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(48, 13);
+            this.label10.TabIndex = 24;
+            this.label10.Text = "= 1 USD";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label12.Location = new System.Drawing.Point(753, 75);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(48, 13);
+            this.label12.TabIndex = 25;
+            this.label12.Text = "= 1 USD";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label13.Location = new System.Drawing.Point(753, 127);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(48, 13);
+            this.label13.TabIndex = 26;
+            this.label13.Text = "= 1 USD";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label14.Location = new System.Drawing.Point(753, 101);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(48, 13);
+            this.label14.TabIndex = 27;
+            this.label14.Text = "= 1 USD";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.btnApplyChanges);
+            this.Controls.Add(this.txtKhrRate);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtEurRate);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtBahRate);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.txtAudRate);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.txtKhr);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtEur);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtBah);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtAud);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtUsd);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Title);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MianForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Title;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtUsd;
+        private System.Windows.Forms.TextBox txtAud;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtBah;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtEur;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtKhr;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnApplyChanges;
+        private System.Windows.Forms.TextBox txtKhrRate;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtEurRate;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtBahRate;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtAudRate;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExchangeRate
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            txtKhrRate.Text = khrRate.ToString();
+            txtEurRate.Text = eurRate.ToString();
+            txtBahRate.Text = bahRate.ToString();
+            txtAudRate.Text = audRate.ToString();
+        }
+        // Main exchange rate
+        double usdRate = 1;
+        double khrRate = 4128;
+        double eurRate = 0.96;
+        double bahRate = 33.20;
+        double audRate = 1.11;
+
+
+        void calcutate(double value, TextBox ignoreTextbox)
+        {
+            Dictionary<TextBox, double> textBoxesAndRates = new Dictionary<TextBox, double>
+            {
+                { txtAud, audRate },
+                { txtKhr, khrRate },
+                { txtEur, eurRate },
+                { txtBah, bahRate },
+                { txtUsd, usdRate }
+            };
+
+            foreach (var textBox in textBoxesAndRates.Keys)
+            {
+                if (ignoreTextbox != textBox)
+                {
+                    textBox.Text = (value * textBoxesAndRates[textBox]).ToString("0.##");
+                }
+            }
+        }
+
+        void clear()
+        {
+            txtUsd.Clear();
+            txtKhr.Clear();
+            txtEur.Clear();
+            txtBah.Clear();
+            txtAud.Clear();
+
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            clear();
+        }
+
+        private void btnApplyChanges_Click(object sender, EventArgs e)
+        {
+            if (double.TryParse(txtKhrRate.Text, out khrRate) &&
+                double.TryParse(txtEurRate.Text, out eurRate) &&
+                double.TryParse(txtBahRate.Text, out bahRate) &&
+                double.TryParse(txtAudRate.Text, out audRate))
+            {
+                clear();
+                MessageBox.Show("Exchange rate successfully updated.");
+            }
+            else
+            {
+                MessageBox.Show("Invalid input. Please enter valid numeric values.");
+            }
+        }
+
+        private void txtUsd_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Back)
+            {
+                clear();
+            }
+            else if (double.TryParse(txtUsd.Text, out double val))
+            {
+                calcutate(val, txtUsd);
+            }
+        }
+
+        private void txtKhr_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Back)
+            {
+                clear();
+            }
+            else if (double.TryParse(txtKhr.Text, out double khrInput))
+            {
+                double usdRate = khrInput / khrRate;
+                calcutate(usdRate, txtKhr);
+            }
+        }
+
+        private void txtEur_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Back)
+            {
+                clear();
+            }
+            else if (double.TryParse(txtEur.Text, out double eurInput))
+            {
+                double usdRate = eurInput / eurRate;
+                calcutate(usdRate, txtEur);
+            }
+        }
+
+        private void txtBah_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Back)
+            {
+                clear();
+            }
+            else if (double.TryParse(txtBah.Text, out double bahInput))
+            {
+                double usdRate = bahInput / bahRate;
+                calcutate(usdRate, txtBah);
+            }
+        }
+
+        private void txtAud_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Back)
+            {
+                clear();
+            }
+            else if (double.TryParse(txtAud.Text, out double audInput))
+            {
+                double usdRate = audInput / audRate;
+                calcutate(usdRate, txtAud);
+            }
+        }
+    }
+}
